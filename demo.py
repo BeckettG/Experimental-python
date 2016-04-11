f1efaf8cbb4e97bfc7cf57aaa137b2844880e92b
glass <glass@glasss-MacBook-Air.local>
2016-04-10 18:19:25 -0700

    inital commit

---
+++ demo.py
@@ -0,0 +1,18 @@
+import turtle
+import time
+
+
+t = turtle.Pen()
+
+t.forward(90)
+t.left(60)
+t.forward(78)
+
+time.sleep(4)
+
+#if this doesnt work please tell me
+
+t.forward(89)
+t.down()
+t.left(120)
+t.forward(100)
